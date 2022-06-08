bab3c064723ffa9028c5cd602a7bd685447e32b7
mikaelhen <mikaelhen28@gmail.com>
2022-06-08 08:42:27 +0200

    maj

---
+++ index.php
@@ -0,0 +1,15 @@
+    <?php
+    include "assets/includes/head.php";
+    include "assets/includes/navbar.php";
+
+    ?>
+    <!-- accueil -->
+    <section class="accueil bg-dark d-flex w-100 h-100 flex-column justify-content-center align-items-center">
+<!-- <img src="../Certfif/assets/img/projet.jpg"> -->
+<h1 class="display-1 text-black text-center">Lorem ipsum dolor sit amet.</h1>
+<p class="lead text-center text-black">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit unde voluptates obcaecati, architecto placeat beatae nisi maxime velit earum animi!</p>
+
+    </section>
+</body>
+
+</html>
